99584fd287043c53cd2dbdd800df0c3183988ead
krayddy <50516437+krayddy@users.noreply.github.com>
2020-12-16 21:33:23 +0500

    Add files via upload

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,17 +1,29 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    for (i <- seq.length - 1 to 0 by -1)
+      yield seq(i)
+  }
 
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    if (idx == 0)
+      0
+    else if (idx == 1 || idx == 2)
+      1
+    else
+      fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    if (idx == 0) Seq(0)
+    else if (idx == 1) Seq(0, 1)
+    else if (idx == 2) Seq(0, 1, 1)
+    else {
+      val temp = fibonacci(idx - 1)
+      temp :+ temp(temp.length - 1) + temp(temp.length - 2)
+    }
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +31,45 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+
+  /*
+
+   */
+  def morse(text: String): String = {
+    var l = ""
+    for (i <- 0 until text.length)
+    {
+      if (MORSE.contains(text(i).toString.toUpperCase())) {
+        if (i > 0) {
+          if (text(i - 1).isLetter || text(i - 1) == ' ')
+            l += " "
+        }
+        if (text(i).isLower) {
+          l += MORSE(text(i).toString.toUpperCase()).toLowerCase()
+        }
+        else {
+          l += MORSE(text(i).toString.toUpperCase()).toUpperCase()
+        }
+      }
+      else {
+        if (text(i) == ' ')
+          l += " "
+        l += text(i)
+      }
+    }
+    l
+  }
 
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = {
+      text.split("(?=[!. ,?])|(?<=[ ])").map(x => if (x != "")
+        {
+      if (x.charAt(0).isUpper) {
+        x.toLowerCase.reverse.capitalize
+      }
+      else {
+        x.reverse
+      }}).mkString("")
+  }
 
 }
